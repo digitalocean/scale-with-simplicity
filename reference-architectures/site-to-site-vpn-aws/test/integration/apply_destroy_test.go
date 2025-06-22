aa5dacd77cabef1c1396a080ba91942cab771279
Joe Keegan <jkeegan@digitalocean.com>
2025-06-22 10:14:33 -0400

    inital s2s-vpn integration test

--- reference-architectures/site-to-site-vpn-aws/test/integration/apply_destroy_test.go
+++ reference-architectures/site-to-site-vpn-aws/test/integration/apply_destroy_test.go
@@ -2,10 +2,8 @@ package integration
 
 import (
 	"fmt"
-	"github.com/digitalocean/scale-with-simplicity/test/constant"
 	"github.com/digitalocean/scale-with-simplicity/test/helper"
 	"github.com/gruntwork-io/terratest/modules/files"
-	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
@@ -13,62 +11,29 @@ import (
 
 	"strings"
 	"testing"
-	"time"
 )
 
 func TestApplyAndDestroy(t *testing.T) {
 	t.Parallel()
-	testNamePrefix := strings.ToLower(random.UniqueId())
+	// Generate unique prefix. K8s resources names cannot start with a number
+	testNamePrefix := fmt.Sprintf("test-%s", strings.ToLower(random.UniqueId()))
+
+	// Copy code and tfvars
 	testDir := test_structure.CopyTerraformFolderToTemp(t, "../..", "./terraform")
-	err := files.CopyFile("../test.tfvars", filepath.Join(testDir, "test.tfvars"))
-	if err != nil {
+	if err := files.CopyFile("../test.tfvars", filepath.Join(testDir, "test.tfvars")); err != nil {
 		t.Fatalf("Failed to copy tfvars file: %v", err)
 	}
 
-	client := helper.CreateGodoClient()
-	testDomainFqdn := helper.CreateTestDomain(client, constant.TestRootSubdomain, testNamePrefix)
-	sshKey := helper.CreateSshKey(client, testNamePrefix)
+	// Configure Terraform options
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: testDir,
 		MixedVars: []terraform.Var{
 			terraform.VarFile("test.tfvars"),
 			terraform.VarInline("name_prefix", testNamePrefix),
-			terraform.VarInline("domain", testDomainFqdn),
-			terraform.VarInline("ssh_key", sshKey.Name)},
+		},
 		NoColor: true,
 	})
+
 	defer helper.TerraformDestroyVpcWithMembers(t, terraformOptions)
-	defer helper.DeleteTestDomain(client, constant.TestRootSubdomain, testNamePrefix)
-	//defer helper.DeleteSshKey(client, sshKey.ID)
-
 	terraform.InitAndApply(t, terraformOptions)
-
-	glbFqdn := terraform.Output(t, terraformOptions, "glb_fqdn")
-	rlbFqdns := terraform.OutputList(t, terraformOptions, "rlb_fqdns")
-	urls := []string{
-		fmt.Sprintf("http://%s", glbFqdn),
-	}
-	for _, fqdns := range rlbFqdns {
-		urls = append(urls, fmt.Sprintf("http://%s", fqdns))
-	}
-
-	// give time for everything to come up
-	time.Sleep(30 * time.Second)
-	validateResponse := func(statusCode int, body string) bool {
-		return statusCode == 200 && strings.Contains(body, "Region:")
-	}
-
-	maxRetries := 12
-	timeBetweenRetries := 5 * time.Second
-
-	for _, url := range urls {
-		http_helper.HttpGetWithRetryWithCustomValidation(
-			t,
-			url,
-			nil,
-			maxRetries,
-			timeBetweenRetries,
-			validateResponse,
-		)
-	}
 }
