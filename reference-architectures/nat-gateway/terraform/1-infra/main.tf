a618aa31d2b74269f1d2ea6ef4c2b0d40d9ef9a0
jx <jai8.jacob@gmail.com>
2025-10-09 15:45:09 +0530

    ra-nat-gateway:  fixes to attributes, tf vars and cloud-config

--- reference-architectures/nat-gateway/terraform/1-infra/main.tf
+++ reference-architectures/nat-gateway/terraform/1-infra/main.tf
@@ -33,24 +33,27 @@ resource "digitalocean_vpc_nat_gateway" "this" {
 
 # Resource 3: Droplet(non K8 host)
 resource "digitalocean_droplet" "non-k8-host-droplet" {
-  image = "fedora-42-x64" 
-  size = "s-1vcpu-512mb-10gb"
+  image = var.droplet_image 
+  size = var.droplet_size 
   name = "non-k8-host-droplet"
   vpc_uuid = digitalocean_vpc.ra-nat-gateway-vpc.id
-  user_data = templatefile("${path.module}/cloud-init-fedora.yaml", {
-    nat_gateway_ip = [for vpc in digitalocean_vpc_nat_gateway.this.vpcs : vpc.gateway_ip][0]
-  })
+  user_data = file("${path.module}/cloud-config.yaml")
   depends_on = [digitalocean_vpc_nat_gateway.this]
 }
 
 # Resource 4: K8 cluster
 resource "digitalocean_kubernetes_cluster" "k8-cluster" {
-  name = "foo"
+  name = "k8-cluster"
   region = var.region
   version = "latest"
+
+  routing_agent {
+    enabled = true
+  }
+
   node_pool {
     name = "worker-pool"
     size = "s-2vcpu-2gb"
-    node_count = 2
+    node_count = var.node_count
   }
 }
--- reference-architectures/nat-gateway/terraform/1-infra/variables.tf
+++ reference-architectures/nat-gateway/terraform/1-infra/variables.tf
@@ -9,3 +9,20 @@ variable "nat_gateway_name" {
   type = string
   default = "nat-gateway"
 }
+
+# Resource 3: Droplet(non K8 host) related
+variable "droplet_image" {
+  type = string
+  default = "ubuntu-24-04-x64"
+}
+
+variable "droplet_size" {
+  type = string
+  default = "s-1vcpu-512mb-10gb"
+}
+
+# Resource 4: K8 cluster related
+variable "node_count" {
+  type = number
+  default = 2
+}
